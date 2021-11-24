a61ac28a9b38bb66be2b09b83d0ab30fcbc7d2b6
Sae Shikiji <saeshikiji@gmail.com>
2021-11-24 11:35:15 +0900

    Added smooth scroll animation

--- source/assets/javascripts/index.js
+++ source/assets/javascripts/index.js
@@ -1,1 +1,11 @@
 import 'bootstrap';
+
+document.querySelectorAll('.hashscroll').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
